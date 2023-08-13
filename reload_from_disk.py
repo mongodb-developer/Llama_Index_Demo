766209e80f7d18e9c8a48643f014745690ac478e
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-08-13 15:02:18 -0500

    Create reload_from_disk.py

---
+++ reload_from_disk.py
@@ -0,0 +1,6 @@
+from llama_index import StorageContext, load_index_from_storage
+
+# rebuild storage context
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
+# load index
+index = load_index_from_storage(storage_context)
